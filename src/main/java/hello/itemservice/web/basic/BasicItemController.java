96e7a8c499739615bcdc677a44d209923a0bfc86
jiwoo <zbqmgldjfh@gmail.com>
2021-10-21 21:49:26 +0900

    서비스 HTML구현 1) bootstrap 적용 2) thymeleaf 적용

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,36 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findALl();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    // 테스트 용 데이터!
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
