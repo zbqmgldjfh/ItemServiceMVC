1da6d2b0ec1196a3e2982ba94b2630d184a353ff
jiwoo <zbqmgldjfh@gmail.com>
2021-10-24 11:41:28 +0900

    PRG pattern 적용
    Post/Redirect/Get 방식을 적용한 addItemV4 컨트롤러 구현

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -54,11 +54,16 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item){
         itemRepository.save(item);
-        //model.addAttribute("item", item); 자동 추가 생략 가능
         return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item){
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
